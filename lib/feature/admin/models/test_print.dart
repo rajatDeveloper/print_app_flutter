ef29857f37bc2ea5bc9219e21408618ffd34c5bb
Rajat <85779110+rajatDeveloper@users.noreply.github.com>
2024-10-03 19:45:59 +0530

    Update test_print.dart

--- lib/feature/admin/models/test_print.dart
+++ lib/feature/admin/models/test_print.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 import 'package:flutter/services.dart';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart';
+import 'package:intl/intl.dart';
 import 'package:print_app/feature/print_cart/models/print_cart_model.dart';
 import 'package:print_app/printerenum.dart';
 
@@ -17,6 +18,17 @@ class TestPrint {
   // Method to calculate total quantity
   int _getTotalQuantity(List<PrintCartModel> printCartList) {
     return printCartList.fold(0, (total, e) => total + (e.quantity ?? 0));
+  }
+
+  String formatDate() {
+    // Parse the date string into a DateTime object
+    DateTime dateTime = DateTime.now();
+
+    // Define the desired format
+    DateFormat formatter = DateFormat('d MMM yyyy h:mm a');
+
+    // Format the DateTime object to the desired string format
+    return formatter.format(dateTime);
   }
 
   // Method to calculate total amount
@@ -41,17 +53,22 @@ class TestPrint {
     bluetooth.printCustom("KHANAURI JUNCTION", boldLarge, centerAlign);
     bluetooth.printCustom(
         "PATRAN - NARWANA ROAD KHANAURI - 148027", medium, centerAlign);
-    bluetooth.printLeftRight("Anil", "Ankit", medium);
-    bluetooth.printLeftRight("8396833173", "7495014651", medium);
+    bluetooth.printLeftRight("Anil: 8396833173", "Ankit: 7495014651", medium);
+
+    bluetooth.printNewLine();
+    //Date
+    bluetooth.printCustom(formatDate(), medium, centerAlign);
+
     bluetooth.printNewLine();
 
     // Print Invoice Items
     for (var item in invoiceItem) {
-      bluetooth.printCustom("${item.product!.name}", medium, leftAlign);
+      bluetooth.printCustom("${item.product!.name}", bold, leftAlign);
       bluetooth.printLeftRight(
         "${item.quantity} x ${item.product!.price!} =",
-        "${(double.parse(item.product!.price!) * (item.quantity ?? 0)).toStringAsFixed(2)}",
-        medium,
+        (double.parse(item.product!.price!) * (item.quantity ?? 0))
+            .toStringAsFixed(2),
+        bold,
       );
     }
     bluetooth.printNewLine();
@@ -71,6 +88,8 @@ class TestPrint {
 
     // Footer
     bluetooth.printCustom("Thank you, Visit again", bold, centerAlign);
+    bluetooth.printNewLine();
+    bluetooth.printNewLine();
     bluetooth.printNewLine();
 
     // Cut Paper (with delay to ensure paper holds)
